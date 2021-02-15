055bfcfd599ec0ab29932c8ba257e381eca4c8f9
Alex Reed <alexreed100@gmail.com>
2021-02-15 17:46:28 -0500

    fix ingredient formatting

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,12 +5,16 @@ const Recipe = ({ title, calories, image, ingredients }) => {
     return (
         <div className={style.recipe}>
             <h1 >{title}</h1>
-            <ol>
-                {ingredients.map(ingredient =>(
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-            <p>{calories}</p>
+            <div className={style.left}>
+                <p>Calories: {calories}</p>
+                <h2 >Instructions:</h2>
+                <ol>
+                    {ingredients.map(ingredient => (
+                        <li>{ingredient.text}</li>
+                    ))}
+                </ol>
+            </div>
+
             <img className={style.image} src={image} alt="" />
         </div>
     );
