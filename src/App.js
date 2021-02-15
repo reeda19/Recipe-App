9cfa157d7d71d45864cef426e88c2535e1bb3f24
Alex Reed <alexreed100@gmail.com>
2021-02-15 16:43:12 -0500

    implement search functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import Recipe from './Recipe';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "3c471076"
+  const APP_KEY = "74d9d8965ef5d3651b75543e254649ba"
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
+  useEffect(() => {
+    getRecipes();
+  }, [query])
+
+  // any external data requests need to sue async/await or with promises (.then)
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">
+          Search
+         </button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories}
+          image={recipe.recipe.image} />
+      ))}
     </div>
   );
 }
