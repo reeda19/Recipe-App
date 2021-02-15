686df2b43e712811640a64816b28c10c22f4ed86
Alex Reed <alexreed100@gmail.com>
2021-02-15 17:17:26 -0500

    place recipes in cards

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './recipe.module.css'
 
 const Recipe = ({ title, calories, image, ingredients }) => {
     return (
-        <div>
-            <h1>{title}</h1>
+        <div className={style.recipe}>
+            <h1 >{title}</h1>
             <ol>
                 {ingredients.map(ingredient =>(
                     <li>{ingredient.text}</li>
